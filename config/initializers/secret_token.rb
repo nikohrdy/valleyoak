e0f4eeedbdfb982f7faaa96bef8461891bb6335c
Niko Hrdy <nikohrdy@gmail.com>
2014-02-23 23:36:02 +0000

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Valleyoak::Application.config.secret_key_base = '4ec678497cae14dc94ddbabe956b195502fc985f8154cef23f6c077bc11650dc6b1f791123f700144fa3c4de61e21329da02e59e45b74ab94619a10a67c66dc3'
